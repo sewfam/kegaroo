71861969f89ae23c3c1c4ea6cc084697b021f79f
William Seward <willspotsew@Williams-MacBook-Air.local>
2024-08-19 21:52:16 -0700

    new look

---
+++ script.js
@@ -0,0 +1,24 @@
+// Countdown Timer Script
+const countdown = () => {
+    const eventDate = new Date('January 19, 2025 06:00:00').getTime();
+    const now = new Date().getTime();
+    const timeLeft = eventDate - now;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+
+    if (timeLeft < 0) {
+        clearInterval(timer);
+        document.getElementById('countdown').innerHTML = "The event has started!";
+    }
+};
+
+const timer = setInterval(countdown, 1000);
+countdown();
